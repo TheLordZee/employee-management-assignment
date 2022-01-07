44ffb7b29c22d0914ccf3e73fe309e2da9186042
TheLordZee <l0rclz33@gmail.com>
2022-01-06 19:00:46 -0500

    Delete Employee route

--- src/main/java/com/hcl/management/app/DeleteEmployee.java
+++ src/main/java/com/hcl/management/app/DeleteEmployee.java
@@ -1,10 +1,42 @@
 package com.hcl.management.app;
 
-public class DeleteEmployee {
+import java.util.Scanner;
 
+import com.hcl.crud.EmployeesAPI;
+
+public class DeleteEmployee {
+	public static String input = "";
+	
 	public static void display() {
-		// TODO Auto-generated method stub
-		
+		Scanner scan = new Scanner(System.in);
+		boolean confirm = false;
+		while(!input.equals("RETURN")) {
+			System.out.println("Please enter the ID of the employee who's records you wish to delete or 'RETURN' to go back:");
+			input = scan.nextLine();
+			if(input.equals("QUIT")) {
+				App.quit();
+			} else {
+				try{
+					int id = Integer.parseInt(input);
+					while(!confirm) {
+						System.out.println("Confirm you want to delete all records for Employee with ID " + id + " 'y' or 'n'");
+						input = scan.nextLine();
+						if(input.equals("y")) {
+							confirm = true;
+							EmployeesAPI.delete(id);
+						}else if(!input.equals("n")) {
+							System.out.println("Invalid input.");
+						}else {
+							confirm = true;
+						}
+					}
+					confirm = false;
+					
+				}catch(Exception e) {
+					System.out.println("Employee with id " + input + " does not exist.");
+				}
+			}
+		}
 	}
 
 }
